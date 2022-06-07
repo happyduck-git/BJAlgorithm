163beacfcebd9ad0dd2e9fc622ed44b4fd84aed0
GG <rkrudtls@gmail.com>
2022-06-07 22:09:31 +0900

    좌표정렬하기 주석 추가 및 출력 형식 변경

--- src/좌표정렬하기5.java
+++ src/좌표정렬하기5.java
@@ -5,18 +5,19 @@ public class 좌표정렬하기5 {
 
      /*
        출력: BufferedWriter 사용
-       시간: 976ms
+       시간: 892ms
      */
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
+        //Sort하기 위한 ArrayList 선언 및 생성
         ArrayList<Integer[]> arrayList = new ArrayList<>();
 
-
+        //n개 줄 input 2d array에 받기
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             Integer[] list = new Integer[2];
@@ -24,11 +25,21 @@ public class 좌표정렬하기5 {
             list[1] = Integer.parseInt(st.nextToken());
             arrayList.add(list);
         }
+        /* 예시:
+            arrayList = {
+                         {3,4},
+                         {1,1},
+                         {1,-1},
+                         {2,2},
+                         {3,3}
+                       }
+        */
 
+        //별도로 comparator 익명 클래스 선언하여 파라미터로 넘겨줌!
         Collections.sort(arrayList, comp);
 
         for(Integer[] ints : arrayList) {
-            bw.write(Arrays.toString(ints).replace(",","").replace("[","").replace("]","")+"\n");
+            bw.write(ints[0] + " " + ints[1] + "\n");
         }
         br.close();
         bw.flush();
