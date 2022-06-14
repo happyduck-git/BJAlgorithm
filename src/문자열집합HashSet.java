cde11003eaab458a2da80d606036473bc87d22bc
GG <rkrudtls@gmail.com>
2022-06-15 00:02:40 +0900

    문자열 집합, 스택수열 주석 추가

--- src/문자열집합HashSet.java
+++ src/문자열집합HashSet.java
@@ -1,14 +1,18 @@
 import java.io.*;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
-public class 문자열집합 {
+public class 문자열집합HashSet {
+
     /*14425번 문자열 집합
-      Array 사용
-      2672ms
+      HashSet 사용
+      424ms
      */
+
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
 
         //n m input
@@ -16,30 +20,23 @@ public class 문자열집합 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-//        n개의 문자열 array에 넣기
-        String[] nStrings = new String[n];
+        // n개의 문자열 hashSet에 넣기
+        HashSet<String> nStrings = new HashSet<>();
         for(int i = 0; i < n; i++) {
-            nStrings[i] = br.readLine();
+            nStrings.add(br.readLine());
         }
 
-
-//        m개의 문자열 array에 넣기
-        String[] mStrings = new String[m];
-        for(int i = 0; i < m; i++) {
-            mStrings[i] = br.readLine();
-
-        }
-
-        //포함된 수 세기 위한 count 생성
         int count = 0;
+        //set은 index로 비교가 어려우므로 바로 br.readLine() input 받으면서 비교하기!
         for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(mStrings[i].equals(nStrings[j])) count++;
-            }
+            String temp = br.readLine();
+            if(nStrings.contains(temp)) count++;
         }
 
+        br.close();
         bw.write(count + "");
         bw.flush();
         bw.close();
+
     }
 }
--- src/스택수열.java
+++ src/스택수열.java
@@ -6,21 +6,24 @@ public class 스택수열 {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
+
         int n = Integer.parseInt(br.readLine());
-        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<Integer> nums = new ArrayList<>(); //1부터 n까지 수 담기
         for(int i = 1; i <= n; i++) {
             nums.add(i);
         }
 
-        ArrayList<Integer> sample = new ArrayList<>();
+        ArrayList<Integer> sample = new ArrayList<>(); //input으로 들어오는 수 담기
         for(int i = 0; i < n; i++) {
             sample.add(Integer.parseInt(br.readLine()));
         }
-        StringBuilder sb = new StringBuilder();
-        ArrayList<Integer> temp = new ArrayList<>();
-        ArrayList<Integer> answer = new ArrayList<>();
-        int index = 0;
 
+        StringBuilder sb = new StringBuilder(); //+,- 기호 append할 sb 생성
+        ArrayList<Integer> temp = new ArrayList<>(); //임시로 담아 숫자 둘 stack 역할하는 arrayList 생성 (push)
+        ArrayList<Integer> answer = new ArrayList<>(); //temp에서 pop해서 나오는 숫자 담을 arrayList 생성
+        int index = 0; //inner for loop의 시작점 설정 위한 index
+
+        //이중 for loop으로 숫자 크기 비교하여 push or pop 수행
         for(int i = 0; i < n; i++) {
             for(int j = index; j < n; j++) {
                 if(sample.get(i) >= nums.get(j)) {
@@ -35,6 +38,8 @@ public class 스택수열 {
                 }
             }
         }
+
+        //temp에 남아있는 수 모두 Last in부터 꺼내기
         if(temp.size() != 0) {
             while(temp.size() != 0) {
                 answer.add(temp.get(temp.size() - 1));
@@ -43,8 +48,9 @@ public class 스택수열 {
             }
         }
 
+        //실제 나온 결과값인 answer와 input으로 받은 값 sample이 일치하는지 확인
         if(answer.equals(sample)) {
-            bw.write(sb.substring(0,sb.length()-1));
+            bw.write(sb.substring(0,sb.length()-1)); //맨 뒤에 붙어있는 \n 제거하기 위해 .substring 사용
         } else {
             bw.write("NO");
         }
