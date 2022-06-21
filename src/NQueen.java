5314bde095fc1c1987785881a84f997473322977
GG <rkrudtls@gmail.com>
2022-06-22 00:57:21 +0900

    N-Queen 주석추가

--- src/NQueen.java
+++ src/NQueen.java
@@ -2,7 +2,8 @@ import java.io.*;
 
 public class NQueen {
     //9663번 N-Queen
-
+    //어차피 같은 행이나 열에는 놓을 수 없음
+    //index를 column, index에 들어있는 값을 row로 생각!
     static int[] arr;
     static int n;
     static int count = 0;
@@ -21,6 +22,7 @@ public class NQueen {
     }
 
     public static void nQueen(int depth) {
+        //Base case
         if(depth == n) {
             count++;
             return;
@@ -29,11 +31,13 @@ public class NQueen {
         for(int i = 0; i < n; i++) {
             arr[depth] = i;
             if(isAvailable(depth)) {
+                //한개 놓고 다른 한개 놓고... n개 놓을 때까지 반복
                 nQueen(depth + 1);
             }
         }
     }
 
+    //int col에서 가능한 자리는 어디가 있는지 확인하기
     public static boolean isAvailable(int col) {
         for(int i = 0; i < col; i++) {
             if(arr[col] == arr[i]) {
