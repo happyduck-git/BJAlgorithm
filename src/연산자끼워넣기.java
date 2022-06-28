3385e6fb3ddd7fb8e296b752cff03a7e2013c0f9
GG <rkrudtls@gmail.com>
2022-06-29 07:57:32 +0900

    연산자 끼워넣기 주석추가

--- src/연산자끼워넣기.java
+++ src/연산자끼워넣기.java
@@ -12,30 +12,37 @@ public class 연산자끼워넣기 {
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
 
+        /* input 받기 */
         //숫자 개수
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
 
-        //arr 채우기
+            //arr 채우기
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        //operator 채우기
+            //operator 채우기
         st = new StringTokenizer(br.readLine());
         operators = new int[4]; // +, -, *, / 총 4개 종류이므로
         for(int i = 0; i < 4; i++) {
             operators[i] = Integer.parseInt(st.nextToken());
         }
 
+        /* dfs 탐색 */
         dfs(arr[0], 1);
 
-        System.out.println(max);
-        System.out.println(min);
+
+        /* output */
+        br.close();
+        bw.write(max+ "\n" + min);
+        bw.flush();
+        bw.close();
 
     }
 
+    /*dfs*/
     public static void dfs(int num, int index) {
         if(index == n) {
             max = Math.max(max, num);
