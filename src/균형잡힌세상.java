66c3c3e0872af35314299ccac27c59196acdcfba
GG <rkrudtls@gmail.com>
2022-06-28 21:55:37 +0900

    연산자끼워넣기

---
+++ src/균형잡힌세상.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class 균형잡힌세상 {
+}
---
+++ src/연산자끼워넣기.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 연산자끼워넣기 {
+    static int n;
+    static int[] arr;
+    static int[] operators;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+
+        //숫자 개수
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        //arr 채우기
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        //operator 채우기
+        st = new StringTokenizer(br.readLine());
+        operators = new int[4]; // +, -, *, / 총 4개 종류이므로
+        for(int i = 0; i < 4; i++) {
+            operators[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(arr[0], 1);
+
+        System.out.println(max);
+        System.out.println(min);
+
+    }
+
+    public static void dfs(int num, int index) {
+        if(index == n) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            return;
+        }
+        for(int i = 0; i < 4; i++) {
+            if(operators[i] > 0) {
+                operators[i]--;
+
+                if(i==0) {
+                    dfs(num + arr[index], index + 1);
+                } else if(i == 1) {
+                    dfs(num - arr[index], index + 1);
+                } else if(i == 2) {
+                    dfs(num * arr[index], index + 1);
+                } else if(i == 3){
+                    dfs(num / arr[index], index + 1);
+                }
+                    operators[i]++; //연산자 수량 되돌려 놓기!!
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
