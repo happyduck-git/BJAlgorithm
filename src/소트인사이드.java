3ab81aac0a4613cd4b45f5c679c16c74170fc604
GG <rkrudtls@gmail.com>
2022-07-06 00:02:17 +0900

    소트인사이드, 최대힙 주석 추가

--- src/소트인사이드.java
+++ src/소트인사이드.java
@@ -7,15 +7,20 @@ public class 소트인사이드 {
     static int[] counts;
     public static void main(String[] args) throws IOException {
 
-        String input = br.readLine();
+        //0부터 9까지의 인덱스를 숫자로 사용
         counts = new int[10];
 
+        String input = br.readLine();
+
         for(int i = 0; i < input.length(); i++) {
+            //input 받은 String에 있는 수들은 공백으로 구분이 되지 않으므로 charAt 사용
             int curr = input.charAt(i) - '0';
+            //해당 수와 일치하는 인덱스의 값 올려주기
             counts[curr]++;
         }
         br.close();
 
+        //내림차순이 되어야하므로 9부터 0까지 차례로 갯수를 bw에 write해주기
         for(int i = (counts.length - 1); i >= 0; i--) {
             while(counts[i] != 0) {
                 bw.write(i+"");
@@ -24,7 +29,6 @@ public class 소트인사이드 {
         }
         bw.flush();
         bw.close();
-
     }
 
 }
--- src/최대힙.java
+++ src/최대힙.java
@@ -1,47 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class 최대힙 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
 
-        int totalNum = Integer.parseInt(br.readLine());
+        //큰 숫자에 priority를 주기 위해서 Collections.reverseOrder 적용 (default는 작은 숫자에 우선순위가 있음)
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
 
-        LinkedList<Integer> list = new LinkedList<>();
+        //n input
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < totalNum; i++) {
-            int numPassed = Integer.parseInt(br.readLine());
+        //n번 만큼 수가 주어지므로 while문 사용
+        while(n > 0) {
+            n--; //턴 돌면서 하나씩 감소
+            int x = Integer.parseInt(br.readLine()); //input으로 들어오는 수 x에 할당
 
-            if(numPassed == 0) {
-                //if list is empty
-                if(list.size() == 0) {
-                    sb.append("0" + "\n");
-                } else {
-                    Integer max = 0;
-                    for(int j = 0; j < list.size(); j++) {
-                        if(list.get(j) > max) { max = list.get(j);}
-                    }
-
-                    sb.append(max+"").append("\n");
-
-                    list.remove(max);
+            if(x > 0) { //x가 0보다 큰 경우는 priorityQueue에 넣어줌
+                priorityQueue.add(x);
+            } else { //x가 0과 같거나 0보다 작은 경우는 queue size에 따라 다르게 행동
+                if(priorityQueue.size() == 0) { //queue가 빈 경우는 0을 out
+                    sb.append(0 + "\n");
+                } else { //queue에 숫자가 있는 경우는 가장 큰 수를 remove하고 해당 수는 out
+                    sb.append(priorityQueue.remove() + "\n");
                 }
-            } else {
-                    list.add(numPassed);
             }
 
         }
-        System.out.println(sb.toString());
+
+        //string builder 마지막에 있는 \n 제거하고 출력
+        System.out.println(sb.substring(0, sb.length() - 1));
 
     }
 }
 
 
+/*
+    int totalNum = Integer.parseInt(br.readLine());
 
+    Set<Integer> list = new HashSet<>();
 
+        for(int i = 0; i < totalNum; i++) {
+        int numPassed = Integer.parseInt(br.readLine());
+
+        if(numPassed == 0) {
+        //if list is empty
+        if(list.size() == 0) {
+        sb.append("0" + "\n");
+        } else {
+        Integer max = list.stream()
+        .max(Comparator.naturalOrder())
+        .orElse(0);
+
+        sb.append(max+"").append("\n");
+
+        list.remove(max);
+        }
+        } else {
+        list.add(numPassed);
+        }
+
+        }
+        System.out.println(sb.toString());
+
+ */
 
 
 
